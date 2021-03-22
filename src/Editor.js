c68a333345b5f1e4551f54e0d19e71c156eb75f9
Adam Milner <ajmilner85@gmail.com>
2021-03-21 22:59:40 -0400

    Added Editor and Preview components, Marked library

---
+++ src/Editor.js
@@ -0,0 +1,9 @@
+const Editor = () => {
+    return ( 
+        <div className="editor">
+            <textarea>Enter markdown text here...</textarea>
+        </div>
+     );
+}
+ 
+export default Editor;
---
+++ src/Preview.js
@@ -0,0 +1,7 @@
+const Preview = () => {
+    return ( 
+        <div className="preview">Preview text here...</div>
+     );
+}
+ 
+export default Preview;
